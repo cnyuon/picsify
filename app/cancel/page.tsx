a8f7423b4615c71cb88187cd9eb1438b8dfde1df
cnyuon <chioknyuon2020@gmail.com>
2024-06-18 17:07:38 -0700

    cancel and success edit

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function Cancel() {
   return (
     <div className="container mx-auto py-20 justify-center items-center flex flex-col">
-      <h1 className="text-4xl font-bold mb-5 ">Payment Cancelled</h1>
-      <p className="mb-5">Your payment was not successful. Please try again.</p>
+      <h1 className="text-4xl font-bold mb-5 justify-center items-center flex flex-col">Payment Cancelled</h1>
+      <p className="mb-5 justify-center items-center flex flex-col">Your payment was not successful. Please try again.</p>
       <Link href="/buycredits">
-        <p className="text-blue-600">Go back to Buy Credits</p>
+        <p className="text-blue-600 justify-center items-center flex flex-col">Go back to Buy Credits</p>
       </Link>
     </div>
   );
